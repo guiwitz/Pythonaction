15a4ef3d36d5642bc11d2b8004ad879ede3c489d
guiwitz <guiwitz@gmail.com>
2020-10-10 00:22:26 +0200

    added function

--- pythonaction/second_module.py
+++ pythonaction/second_module.py
@@ -15,3 +15,22 @@ def my_second_function(a):
 
     b = 3 * a
     return b
+
+
+def my_third_function(a):
+    """
+    This is my function.
+
+    Parameters
+    ----------
+    a: 2d array
+        This is my array.
+
+    Returns
+    -------
+    b: 2d array
+
+    """
+
+    b = 3 * a
+    return b
