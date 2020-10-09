946eca82eda0638d260fbfb2991627f343d143ab
guiwitz <guiwitz@gmail.com>
2020-10-09 17:02:40 +0200

    initial commit

---
+++ pythonaction/mymodule.py
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+
+def my_function(a):
+    """
+    This is my function.
+
+    Parameters
+    ----------
+    a: 2d array
+        This is my array.
+
+    Returns
+    -------
+    b: 2d array
+
+    """
+    
+    b = 3 * a
+    return b
