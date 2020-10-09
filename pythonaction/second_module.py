60344cf701d6d4af7f75528fcf69a4f8c3b9a7bf
guiwitz <guiwitz@gmail.com>
2020-10-09 21:52:44 +0200

    add second module

---
+++ pythonaction/second_module.py
@@ -0,0 +1,17 @@
+def my_second_function(a):
+    """
+    This is my function.
+
+    Parameters
+    ----------
+    a: 2d array
+        This is my array.
+
+    Returns
+    -------
+    b: 2d array
+
+    """
+
+    b = 3 * a
+    return b
